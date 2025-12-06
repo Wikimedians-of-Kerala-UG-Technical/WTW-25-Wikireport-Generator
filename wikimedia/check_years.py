6239d80157aa942838f5e47c549b20eabe8d0d5b
nandhuts2002 <nandhunandhuts007@gmail.com>
2025-12-06 14:33:26 +0530

    Delete wikimedia directory

--- wikimedia/check_years.py
+++
@@ -1,8 +0,0 @@
-import json
-
-data = json.load(open('malayalam_movies.json'))
-years = [int(m['release_year']) for m in data['movies'] if m.get('release_year') and str(m.get('release_year')).isdigit()]
-print(f'Year range: {min(years)} - {max(years)}')
-
-recent = [m for m in data['movies'] if 2020 <= int(m.get('release_year', '0') or '0') <= 2025]
-print(f'Movies 2020-2025: {len(recent)}')
--- wikimedia/run.bat
+++
@@ -1,2 +0,0 @@
-cd /d \"c:\Users\Abel Anil\Downloads\wikimedia\wikimedia\"  
-python download_movies.py  
